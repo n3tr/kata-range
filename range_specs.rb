6373d9876ce74963328773a6d98c86124000e518
Weerasak Chongnguluam <singpor@gmail.com>
2013-12-17 00:33:00 +0700

    Use Regex split range

--- range_specs.rb
+++ range_specs.rb
@@ -8,39 +8,21 @@ class Range
 	end
 
 	def to_set()
-		
-		data = @range.split(',')
-
-		start = get_start_bound(data[0]) 
-		last = get_last_bound(data[data.size-1])
-
-		create_set(start,last)
+		data = split_range()
+		create_set(data[0],data[1])
 	end
 
-	def get_start_bound(start)
-		start_sign = start[0]
-		start_value = start[1,start.size-1].to_i
-		start_value += 1 if start_sign == '('
-		start_value
-	end
-
-	def get_last_bound(last)
-		last_sign = last[last.size-1]
-		last_value = last[0,last.size-1].to_i
-		last_value -= 1 if last_sign == ')'
-		last_value
-	end
-
+  def split_range()
+    match_data = /^([\[\(])([-[[:digit:]]]+),([-[[:digit:]]]+)([\]\)])$/.match(@range)
+    puts match_data
+    start_value = match_data[2].to_i
+    start_value += 1 if match_data[1] == '('
+    last_value = match_data[3].to_i
+    last_value -= 1 if match_data[4] == ')'
+    [start_value, last_value]
+  end
+	
 	def create_set(start_value,last_value)
-		# build_string = ""
-
-		# (start_value..last_value).each { |i| 
-		# 	build_string += "," if i != start_value
-		# 	build_string += i.to_s
-		# }
-
-		# "{#{build_string}}"
-
 		range_array = Array.new
 		
 		(start_value..last_value).each { |i| range_array.push(i) }
